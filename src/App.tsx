afef1928317cc42620598ef4fd285d3ce62b8abb
Brett Abramczyk <bjabramczyk@gmail.com>
2022-02-01 12:39:29 +0100

    group systems by type

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import "./App.css";
 import SAMPLE_DATA from "./sample-data.json";
 
+// TODO: Move these to a separate file?
+type System = typeof SAMPLE_DATA[number];
+// TODO: Make this more explicit, with this assumption?
+type SystemType = System["system_type"];
+
 function App() {
-  return <div className="App">{JSON.stringify(SAMPLE_DATA, null, 2)}</div>;
+  // TODO: Refactor this. It's extremely ugly and I hate it
+  const systemsByType: Record<SystemType, System[]> = SAMPLE_DATA.reduce<
+    Record<SystemType, System[]>
+  >((systemsByType, system) => {
+    const existingSystemsForType = systemsByType[system.system_type] ?? [];
+    return {
+      ...systemsByType,
+      [system.system_type]: [...existingSystemsForType, system],
+    };
+  }, {});
+
+  return <div className="App">{JSON.stringify(systemsByType, null, 2)}</div>;
 }
 
 export default App;
