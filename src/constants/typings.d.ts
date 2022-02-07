191c7d03b49c963ac0ed56749f4f9f3a227d74e2
Brett Abramczyk <bjabramczyk@gmail.com>
2022-02-07 11:58:05 +0100

    fixup

--- src/constants/typings.d.ts
+++ src/constants/typings.d.ts
@@ -2,6 +2,7 @@ import { SAMPLE_DATA } from "./sample-data";
 
 export type System = typeof SAMPLE_DATA[number];
 // TODO: Make this more explicit, with this assumption?
+// TODO: Put this on a namespace? (i.e. `System.Type`)
 export type SystemType = System["system_type"];
 export type DataUseKey = typeof DATA_USES[number]["privacy_key"];
 export type DataCategoryKey = typeof DATA_CATEGORIES[number]["privacy_key"];
